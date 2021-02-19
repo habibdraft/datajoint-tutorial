1268a213fd30abaa67563fff338d626b93c20565
Nazia Habib <habib.nazia@gmail.com>
2021-02-19 11:46:11 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,3 +3,5 @@ import datajoint as dj
 dj.config['database.host'] = 'tutorial-db.datajoint.io'
 dj.config['database.user'] = 'nhabib'
 dj.config['database.password'] = None #removed
+
+dj.config.save_global()
